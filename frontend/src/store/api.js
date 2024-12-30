2d5acc815290f988ee9acea16d58a140a9f52c90
wangbertha <wangbertha@gmail.com>
2024-12-30 01:10:03 -0600

    Define cors and base URL based on dev/prod mode

--- frontend/src/store/api.js
+++ frontend/src/store/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_ADDRESS,
+    baseUrl: import.meta.env.VITE_API_ADDRESS || "http://localhost:3000",
   }),
   endpoints: () => ({}),
   tagTypes: ["Listings"],
